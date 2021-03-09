39bbbcadb2f12fcc95d8f7d2963b57db7108a27f
Tarik EL JABIRI <tarik.eljabiri@gmail.com>
2021-03-09 17:21:45 +0100

    Update Spline.spec.ts

--- specs/Sections/Entities/Entities/Spline.spec.ts
+++ specs/Sections/Entities/Entities/Spline.spec.ts
@@ -6,7 +6,14 @@ describe('Spline', () => {
         instancesCount: 0
     }
     it('should return the subClassName given.', () => {
-        const entity = new Spline([[1904.63, 1452.05, 0], [2000.04, 1569.16, 0], [2262.14, 1890.86, 0]], 3, 0, [0, 0, 0, 0, 0.5, 2, 2, 2, 2], [], []);
+        let points = [
+            [0, 0, 0],
+            [0, 10, 0],
+            [15, 15, 0],
+            [30, 10, 0],
+            [30, 0, 0]
+        ];
+        const entity = new Spline(points, 3, 0, [0, 0, 0, 0, 0.5, 2, 2, 2, 2], [], []);
         dataState.instancesCount++;
         expect(entity.subClassName).toBe('AcDbSpline');
     });
