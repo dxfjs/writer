ab8a86e1cec4c1fe7064e4bbb0bc2788a8d90047
Tarik EL JABIRI <tarik.eljabiri@gmail.com>
2021-03-09 17:19:06 +0100

    Update Spline.spec.ts

--- specs/Sections/Entities/Entities/Spline.spec.ts
+++ specs/Sections/Entities/Entities/Spline.spec.ts
@@ -5,9 +5,9 @@ describe('Spline', () => {
     const dataState = {
         instancesCount: 0
     }
-    /*it('should return the subClassName given.', () => {
-        const entity = new Spline([[1904.63, 1452.05, 0], [2000.04, 1569.16, 0], [2262.14, 1890.86, 0]], 1064, 3, );
-        dataState.instancesCount += 2;
-        expect(entity.subClassName).toBe('AcDbCircle');
-    });*/
+    it('should return the subClassName given.', () => {
+        const entity = new Spline([[1904.63, 1452.05, 0], [2000.04, 1569.16, 0], [2262.14, 1890.86, 0]], 3, 0, [0, 0, 0, 0, 0.5, 2, 2, 2, 2], [], []);
+        dataState.instancesCount++;
+        expect(entity.subClassName).toBe('AcDbSpline');
+    });
 });
