d02e1c7abfdf43bd78758db8a30b887e57a2e50f
Tarik EL JABIRI <tarik.eljabiri@gmail.com>
2021-10-11 02:22:15 +0100

    Adding true color support

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,6 +1,6 @@
 /*
- * For a detailed explanation regarding each configuration property and type check, visit:
- * https://jestjs.io/docs/en/configuration.html
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
  */
 
 export default {
@@ -11,7 +11,7 @@ export default {
   // bail: 0,
 
   // The directory where Jest should store its cached dependency information
-  // cacheDirectory: "C:\\Users\\Tarik EL JABIRI\\AppData\\Local\\Temp\\jest",
+  // cacheDirectory: "/tmp/jest_rs",
 
   // Automatically clear mock calls and instances between every test
   // clearMocks: false,
@@ -23,11 +23,11 @@ export default {
   // collectCoverageFrom: undefined,
 
   // The directory where Jest should output its coverage files
-  // coverageDirectory: "coverage",
+  // coverageDirectory: undefined,
 
   // An array of regexp pattern strings used to skip coverage collection
   // coveragePathIgnorePatterns: [
-  //   "\\\\node_modules\\\\"
+  //   "/node_modules/"
   // ],
 
   // Indicates which provider should be used to instrument code for coverage
@@ -73,10 +73,10 @@ export default {
   // An array of file extensions your modules use
   // moduleFileExtensions: [
   //   "js",
-  //   "json",
   //   "jsx",
   //   "ts",
   //   "tsx",
+  //   "json",
   //   "node"
   // ],
 
@@ -93,7 +93,7 @@ export default {
   // notifyMode: "failure-change",
 
   // A preset that is used as a base for Jest's configuration
-  preset: 'ts-jest',
+  // preset: undefined,
 
   // Run tests from one or more projects
   // projects: undefined,
@@ -137,7 +137,7 @@ export default {
   // snapshotSerializers: [],
 
   // The test environment that will be used for testing
-  testEnvironment: "node",
+  // testEnvironment: "jest-environment-node",
 
   // Options that will be passed to the testEnvironment
   // testEnvironmentOptions: {},
@@ -153,7 +153,7 @@ export default {
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
   // testPathIgnorePatterns: [
-  //   "\\\\node_modules\\\\"
+  //   "/node_modules/"
   // ],
 
   // The regexp pattern or array of patterns that Jest uses to detect test files
@@ -163,7 +163,7 @@ export default {
   // testResultsProcessor: undefined,
 
   // This option allows use of a custom test runner
-  // testRunner: "jasmine2",
+  // testRunner: "jest-circus/runner",
 
   // This option sets the URL for the jsdom environment. It is reflected in properties such as location.href
   // testURL: "http://localhost",
@@ -176,8 +176,8 @@ export default {
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   // transformIgnorePatterns: [
-  //   "\\\\node_modules\\\\",
-  //   "\\.pnp\\.[^\\\\]+$"
+  //   "/node_modules/",
+  //   "\\.pnp\\.[^\\/]+$"
   // ],
 
   // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
--- src/Internals/DXFManager.ts
+++ src/Internals/DXFManager.ts
@@ -1,144 +1,191 @@
-import Tag                      from    "./Tag";
-import DXFInterface             from    "./Interfaces/DXFInterface";
-import NameComponent            from    "./Components/NameComponent";
-import ColorComponent           from    "./Components/ColorComponent";
-import LayerComponent           from    "./Components/LayerComponent";
-import PointComponent           from    "./Components/PointComponent";
-import HandleComponent          from    "./Components/HandleComponent";
-import LineTypeComponent        from    "./Components/LineTypeComponent";
-import StandardComponent        from    "./Components/StandardComponent";
-import TextStyleComponent       from    "./Components/TextStyleComponent";
-import ThicknessComponent       from    "./Components/ThicknessComponent";
-import TrueColorComponent       from    "./Components/TrueColorComponent";
-import EntityTypeComponent      from    "./Components/EntityTypeComponent";
-import SubclassMarkerComponent  from    "./Components/SubclassMarkerComponent";
+import Tag from "./Tag";
+import DXFInterface from "./Interfaces/DXFInterface";
+import NameComponent from "./Components/NameComponent";
+import ColorComponent from "./Components/ColorComponent";
+import LayerComponent from "./Components/LayerComponent";
+import PointComponent from "./Components/PointComponent";
+import HandleComponent from "./Components/HandleComponent";
+import LineTypeComponent from "./Components/LineTypeComponent";
+import StandardComponent from "./Components/StandardComponent";
+import TextStyleComponent from "./Components/TextStyleComponent";
+import ThicknessComponent from "./Components/ThicknessComponent";
+import TrueColorComponent from "./Components/TrueColorComponent";
+import EntityTypeComponent from "./Components/EntityTypeComponent";
+import SubclassMarkerComponent from "./Components/SubclassMarkerComponent";
 
-export default class DXFManager implements DXFInterface
-{
+export default class DXFManager implements DXFInterface {
+  get handle(): string {
+    return this._handle;
+  }
 
-    get handle() : string { return this._handle;  }
+  set handle(value: string) {
+    this._handle = value;
+  }
 
-    set handle(value : string) { this._handle = value; }
+  static trueColor: number = NaN;
 
-    static versions = {
-        R12     :   "AC1009", R13   :   "AC1012", R14   :   "AC1014",
-        R2000   :   "AC1015", R2004 :   "AC1018", R2007 :   "AC1021",
-        R2010   :   "AC1024", R2013 :   "AC1027", //R2018 :   "AC1032"
-    };
-    static colors = {
-        Red     :   1, Green    :   3, Cyan     :   4,
-        Blue    :   5, Magenta  :   6, White    :   7,
-        Black   :   0, Yellow   :   2,
-    }
-    static units = {
-        Unitless            :   0,  Inches      :   1,  Feet        :   2,
-        Miles               :   3,  Millimeters :   4,  Centimeters :   5,
-        Meters              :   6,  Kilometers  :   7,  Microinches :   8,
-        Mils                :   9,  Yards       :   10, Angstroms   :   11,
-        Nanometers          :   12, Microns     :   13, Decimeters  :   14,
-        Decameters          :   15, Hectometers :   16, Gigameters  :   17,
-        AstronomicalUnits   :   18, LightYears  :   19, Parsecs     :   20,
-        USSurveyFeet        :   21, USSurveyInch:   22, USSurveyYard:   23,
-        USSurveyMile        :   24,
-    }
-    static  version         :   string;
-    static  handleSeed      :   number = 0;
-    private _handle         :   string;
-    static  currentLayer    :   string = '0';
+  static setTrueColor = (red: number, green: number, blue: number) => {
+    DXFManager.trueColor = parseInt(
+      "00" +
+        [red, green, blue]
+          .map((component) => {
+            const hex = component.toString(16);
+            return hex.length === 1 ? "0" + hex : hex;
+          })
+          .join(""),
+      16
+    );
+  };
 
-    public constructor() {
-        this._handle = this.handleSeed();
-        DXFManager.version = DXFManager.versions.R2007;
-    }
+  static unsetTrueColor = () => {
+    DXFManager.trueColor = NaN;
+  };
 
-    public isInteger(value: number): boolean {
-        return Number(value) === value && value % 1 === 0;
-    }
+  static versions = {
+    R12: "AC1009",
+    R13: "AC1012",
+    R14: "AC1014",
+    R2000: "AC1015",
+    R2004: "AC1018",
+    R2007: "AC1021",
+    R2010: "AC1024",
+    R2013: "AC1027", //R2018 :   "AC1032"
+  };
+  static colors = {
+    Red: 1,
+    Green: 3,
+    Cyan: 4,
+    Blue: 5,
+    Magenta: 6,
+    White: 7,
+    Black: 0,
+    Yellow: 2,
+  };
+  static units = {
+    Unitless: 0,
+    Inches: 1,
+    Feet: 2,
+    Miles: 3,
+    Millimeters: 4,
+    Centimeters: 5,
+    Meters: 6,
+    Kilometers: 7,
+    Microinches: 8,
+    Mils: 9,
+    Yards: 10,
+    Angstroms: 11,
+    Nanometers: 12,
+    Microns: 13,
+    Decimeters: 14,
+    Decameters: 15,
+    Hectometers: 16,
+    Gigameters: 17,
+    AstronomicalUnits: 18,
+    LightYears: 19,
+    Parsecs: 20,
+    USSurveyFeet: 21,
+    USSurveyInch: 22,
+    USSurveyYard: 23,
+    USSurveyMile: 24,
+  };
+  static version: string;
+  static handleSeed: number = 0;
+  private _handle: string;
+  static currentLayer: string = "0";
 
-    public isFloat(value: number): boolean {
-        return Number(value) === value && value % 1 !== 0;
-    }
+  public constructor() {
+    this._handle = this.handleSeed();
+    DXFManager.version = DXFManager.versions.R2007;
+  }
 
-    public isSupported(version: string): boolean {
-        const fileVersion = parseInt(DXFManager.version.replace('AC', ''));
-        const tagVersion = parseInt(version.replace('AC', ''));
+  public isInteger(value: number): boolean {
+    return Number(value) === value && value % 1 === 0;
+  }
 
-        return tagVersion <= fileVersion;
-    }
+  public isFloat(value: number): boolean {
+    return Number(value) === value && value % 1 !== 0;
+  }
 
-    public handleSeed (): string
-    {
-        DXFManager.handleSeed++;
-        return DXFManager.handleSeed.toString(16).toUpperCase();
-    }
+  public isSupported(version: string): boolean {
+    const fileVersion = parseInt(DXFManager.version.replace("AC", ""));
+    const tagVersion = parseInt(version.replace("AC", ""));
 
-    tags(): Tag[] {
-        return [];
-    }
+    return tagVersion <= fileVersion;
+  }
 
-    public stringify(): string {
-        return this.tags().reduce((str, tag) => {
-            if (this.isSupported(tag.version)) {
-                return `${str}${tag.stringify()}`;
-            }
-            return str;
-        }, '');
-    }
+  public handleSeed(): string {
+    DXFManager.handleSeed++;
+    return DXFManager.handleSeed.toString(16).toUpperCase();
+  }
 
-    public makeColor(index: number): Tag[] {
-        return new ColorComponent(index).tags();
-    }
+  tags(): Tag[] {
+    return [];
+  }
 
-    public makeEntityType(name: string): Tag[] {
-        return new EntityTypeComponent(name).tags();
-    }
+  public stringify(): string {
+    return this.tags().reduce((str, tag) => {
+      if (this.isSupported(tag.version)) {
+        return `${str}${tag.stringify()}`;
+      }
+      return str;
+    }, "");
+  }
 
-    public makeHandle(handle: string): Tag[] {
-        return new HandleComponent(handle).tags();
-    }
+  public makeColor(index: number): Tag[] {
+    return new ColorComponent(index).tags();
+  }
 
-    public makeLayer(layer: string): Tag[] {
-        return new LayerComponent(layer).tags();
-    }
+  public makeEntityType(name: string): Tag[] {
+    return new EntityTypeComponent(name).tags();
+  }
 
-    public makeLineType(lineType: string): Tag[] {
-        return new LineTypeComponent(lineType).tags();
-    }
+  public makeHandle(handle: string): Tag[] {
+    return new HandleComponent(handle).tags();
+  }
 
-    public makeName(name: string, groupCode: number = 2): Tag[] {
-        return new NameComponent(name, groupCode).tags();
-    }
+  public makeLayer(layer: string): Tag[] {
+    return new LayerComponent(layer).tags();
+  }
 
-    public makePoint(
-        x: number, y: number, z: number = 0,
-        is3D: boolean = false, digit: number = 0
-    ): Tag[] {
-        return new PointComponent(x, y, z, is3D, digit).tags();
-    }
+  public makeLineType(lineType: string): Tag[] {
+    return new LineTypeComponent(lineType).tags();
+  }
 
-    public makeStandard(tagsArray: [number, number | string][]): Tag[]
-    {
-        return new StandardComponent(tagsArray).tags();
-    }
+  public makeName(name: string, groupCode: number = 2): Tag[] {
+    return new NameComponent(name, groupCode).tags();
+  }
 
-    public makeSubclassMarker(subclass: string): Tag[]
-    {
-        return new SubclassMarkerComponent(subclass).tags();
-    }
-    public makeTextStyle(textStyleName: string): Tag[]
-    {
-        return new TextStyleComponent(textStyleName).tags();
-    }
+  public makePoint(
+    x: number,
+    y: number,
+    z: number = 0,
+    is3D: boolean = false,
+    digit: number = 0
+  ): Tag[] {
+    return new PointComponent(x, y, z, is3D, digit).tags();
+  }
 
-    public makeThickness(thickness : number): Tag[]
-    {
-        return new ThicknessComponent(thickness).tags();
-    }
-    
-    public makeTrueColor(
-        red: number, green: number, blue: number, digit: number = 0
-    ): Tag[]
-    {
-        return new TrueColorComponent(red, green, blue, digit).tags();
-    }
-};
+  public makeStandard(tagsArray: [number, number | string][]): Tag[] {
+    return new StandardComponent(tagsArray).tags();
+  }
+
+  public makeSubclassMarker(subclass: string): Tag[] {
+    return new SubclassMarkerComponent(subclass).tags();
+  }
+  public makeTextStyle(textStyleName: string): Tag[] {
+    return new TextStyleComponent(textStyleName).tags();
+  }
+
+  public makeThickness(thickness: number): Tag[] {
+    return new ThicknessComponent(thickness).tags();
+  }
+
+  public makeTrueColor(
+    red: number,
+    green: number,
+    blue: number,
+    digit: number = 0
+  ): Tag[] {
+    return new TrueColorComponent(red, green, blue, digit).tags();
+  }
+}
