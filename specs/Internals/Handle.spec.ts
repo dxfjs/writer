8fd4910dceab43c05f27c38149f864419c43638d
EL JABIRI Tarik <tarik.eljabiri@gmail.com>
2022-01-04 19:37:38 +0100

    Update Handle.spec.ts

--- specs/Internals/Handle.spec.ts
+++ specs/Internals/Handle.spec.ts
@@ -1,9 +1,9 @@
-import DXFWriter from '../../src/DXFWriter';
+import DxfWriter from '../../src/DxfWriter';
 import Handle from '../../src/Internals/Handle';
 
 describe('Handle', () => {
 	it('handle sould be 1 ', () => {
-		const dxf = new DXFWriter();
+		const dxf = new DxfWriter();
 		expect(Handle.handleSeed()).toBe('1B');
 	});
 });
