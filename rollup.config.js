075b710136aab44180b2397725318f7b778112d5
Jean Dessane <jean.dessane@gmail.com>
2022-06-20 11:14:37 +0200

    feature: add commonjs build

--- rollup.config.js
+++ rollup.config.js
@@ -2,10 +2,15 @@ import typescript from 'rollup-plugin-typescript2';
 
 export default {
 	input: 'src/index.ts',
-	output: {
-		file: 'lib/index.js',
+	output: [{
+		file: 'lib/esm/index.js',
 		name: 'DxfWriter',
 		format: 'es',
 	},
+		{
+			file: 'lib/index.js',
+			name: 'DxfWriter',
+			format: 'cjs',
+		}],
 	plugins: [typescript()],
 };
