47703b6dac21212f89e40498f793be0152b660f8
EL JABIRI Tarik <tarik.eljabiri@gmail.com>
2022-06-20 19:25:07 +0100

    Update the build process

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,41 @@
 import typescript from 'rollup-plugin-typescript2';
+import dts from 'rollup-plugin-dts';
+import del from 'del';
 
-export default {
-	input: 'src/index.ts',
-	output: [
-		{
-			file: 'lib/esm/index.js',
-			name: 'DxfWriter',
-			format: 'es',
+function folderDelete(folders) {
+	return {
+		name: 'folder-delete',
+		buildEnd() {
+			del.sync(folders, {});
 		},
-		{
-			file: 'lib/index.js',
-			name: 'DxfWriter CommonJS',
-			format: 'cjs',
-			exports: 'named',
-		},
-	],
-	plugins: [typescript()],
-};
+	};
+}
+
+export default [
+	{
+		input: 'src/index.ts',
+		output: [
+			{
+				file: 'lib/esm/index.js',
+				name: 'DxfWriter',
+				format: 'es',
+			},
+			{
+				file: 'lib/index.js',
+				name: 'DxfWriter CommonJS',
+				format: 'cjs',
+				exports: 'named',
+			},
+		],
+		plugins: [
+			typescript({
+				useTsconfigDeclarationDir: true,
+			}),
+		],
+	},
+	{
+		input: './lib/types/index.d.ts',
+		output: [{ file: 'lib/index.d.ts', format: 'es' }],
+		plugins: [folderDelete(['./lib/types']), dts()],
+	},
+];
