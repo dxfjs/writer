2020c018a651bddb5c7738d4bd6bcefd3b57a68b
EL JABIRI Tarik <tarik.eljabiri@gmail.com>
2023-04-14 11:50:47 +0000

    fix: tests

--- src/Sections/EntitiesSection/Entities/SeqEnd.ts
+++ src/Sections/EntitiesSection/Entities/SeqEnd.ts
@@ -1,6 +1,8 @@
+import { Dxfier } from 'Internals/index'
 import Entity from '../Entity'
 
 export class SeqEnd extends Entity {
+  protected override dxfyChild(dx: Dxfier): void {}
   owner: string|undefined
   constructor() {
     super('SEQEND')
