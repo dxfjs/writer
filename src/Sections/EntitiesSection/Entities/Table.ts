c1834cad5e5463b6afdec479c01edb039e9c7cf9
Deepankar <deepankarsama@gmail.com>
2023-07-20 16:33:29 +0530

    Added Cell Alignment

--- src/Sections/EntitiesSection/Entities/Table.ts
+++ src/Sections/EntitiesSection/Entities/Table.ts
@@ -13,6 +13,28 @@ export enum CellType {
   Block = 2,
 }
 
+export enum CellMerged {
+  False = 0,
+  True = 1,
+}
+
+export enum CellAutoFitFlag {
+    False = 0,
+    True = 1,
+}
+
+export enum CellAlignment {
+  TopLeft = 1,
+  TopCenter = 2,
+  TopRight = 3,
+  MiddleLeft = 4,
+  MiddleCenter = 5,
+  MiddleRight = 6,
+  BottomLeft = 7,
+  BottomCenter = 8,
+  BottomRight = 9,
+}
+
 export interface CellOptions extends CommonEntityOptions {
   cellType?: CellType
   cellMerged?: boolean
@@ -22,6 +44,7 @@ export interface CellOptions extends CommonEntityOptions {
   cellRotationValue?: number
   cellText: string
   cellTextHeight: number
+  cellAlignment: CellAlignment
 }
 
 export class Cell {
@@ -33,6 +56,7 @@ export class Cell {
   cellRotationValue: number
   cellText?: string
   cellTextHeight?: number
+  cellAlignment: CellAlignment
 
   constructor(options: CellOptions) {
     this.cellType = options?.cellType || CellType.Text
@@ -43,6 +67,7 @@ export class Cell {
     this.cellRotationValue = options?.cellRotationValue ?? 0
     this.cellText = options?.cellText
     this.cellTextHeight = options?.cellTextHeight
+    this.cellAlignment = options?.cellAlignment ?? CellAlignment.MiddleCenter
   }
 
   dxfy(dx: Dxfier) {
@@ -52,7 +77,8 @@ export class Cell {
     dx.push(175, this.cellBorderWidth)
     dx.push(176, this.cellBorderHeight)
     dx.push(145, this.cellRotationValue)
-    dx.push(91, 262176) // Cell override flag the flag vale is 262176.
+    dx.push(170, this.cellAlignment)
+    dx.push(91, 262177) // Cell override flag the flag vale is 262176 for textHeight override, 262177 for Attachment and textHeight override.
     dx.push(140, this.cellTextHeight) // overriden text height.
     dx.push(301, 'CELL_VALUE')
     dx.push(90, this.cellText ? 4 : 0)
