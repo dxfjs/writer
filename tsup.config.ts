9e9a5531cca6ec8b5b6fa2d03761952d96a5e131
EL JABIRI Tarik <tarik.eljabiri@gmail.com>
2023-09-01 17:57:41 +0100

    chore: update build config (#135)

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,15 +1,18 @@
-import { defineConfig } from "tsup";
+import { Options, defineConfig } from "tsup";
 
-export default defineConfig({
-  entry: ["./src/index.ts"],
-  dts: "./src/index.ts",
-  outDir: "lib",
-  format: ["esm", "cjs"],
-  sourcemap: true,
-  clean: true,
-  outExtension({ format }) {
-    return {
-      js: `.${format}.js`,
-    };
-  },
-});
+function defineOptions({ format, dts }: Pick<Options, "format" | "dts">) {
+  return {
+    entry: ["./src/index.ts"],
+    outDir: "lib",
+    format,
+    dts,
+    sourcemap: true,
+    clean: true,
+  };
+}
+
+const esm: Options = defineOptions({ format: "esm", dts: true });
+const cjs: Options = defineOptions({ format: "cjs" });
+const iife: Options = defineOptions({ format: "iife" });
+
+export default defineConfig([esm, cjs, iife]);
