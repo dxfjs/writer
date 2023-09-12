668e18294e6b277c317aae7fdbfc2c059ab6fad2
EL JABIRI Tarik <tarik.eljabiri@gmail.com>
2023-09-12 18:26:42 +0100

    feat(build): minify and add global name for iife

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,18 +1,25 @@
 import { Options, defineConfig } from "tsup";
 
-function defineOptions({ format, dts }: Pick<Options, "format" | "dts">) {
+type CustomOptions = Pick<Options, "format" | "dts" | "globalName">;
+
+function defineOptions({ format, dts, globalName }: CustomOptions) {
   return {
     entry: ["./src/index.ts"],
     outDir: "lib",
     format,
     dts,
+    globalName,
     sourcemap: true,
     clean: true,
+    minify: true,
   };
 }
 
 const esm: Options = defineOptions({ format: "esm", dts: true });
 const cjs: Options = defineOptions({ format: "cjs" });
-const iife: Options = defineOptions({ format: "iife" });
+const iife: Options = defineOptions({
+  format: "iife",
+  globalName: "DXFWriter",
+});
 
 export default defineConfig([esm, cjs, iife]);
