e2dcc30a5970583782063826d95c91f2b8d37c15
EL JABIRI Tarik <tarik.eljabiri@gmail.com>
2023-09-18 22:20:37 +0100

    refactor!: rename line's properties to match options (#147)

--- src/entities/line.ts
+++ src/entities/line.ts
@@ -11,8 +11,8 @@ export interface LineOptions extends EntityOptions {
 
 export class Line extends Entity {
   thickness: number;
+  start: Point3D;
   end: Point3D;
-  endPoint: Point3D;
   extrusion: Point3D;
 
   override get subClassMarker(): string {
@@ -22,19 +22,19 @@ export class Line extends Entity {
   constructor(options: LineOptions, handle: Handle) {
     super("LINE", handle, options);
     this.thickness = options.thickness ?? 0;
-    this.end = options.start;
-    this.endPoint = options.end;
+    this.start = options.start;
+    this.end = options.end;
     this.extrusion = options.extrusion || extrusion();
   }
 
   override bbox(): BoundingBox {
-    return BBox.line(this.end, this.endPoint);
+    return BBox.line(this.start, this.end);
   }
 
   protected override tagifyChild(mg: TagsManager): void {
     mg.add(39, this.thickness);
-    mg.point(this.end);
-    mg.point(this.endPoint, 1);
+    mg.point(this.start);
+    mg.point(this.end, 1);
     mg.point(this.extrusion, 200);
   }
 }
